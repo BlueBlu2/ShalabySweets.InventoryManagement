0674891008cbc2503965fc6ae3f5cfcd49aab8ad
BlueBlu2 <eng.abdalrhmn.mohamed@gmail.com>
2025-10-06 10:06:47 +0300

    Initial create

---
+++ ShalabySweets.InventoryManagement/Product.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Product
+{
+    private int id;
+    private string name = string.Empty;
+    private string? description;
+
+    private int maxItemsInStock = 0;
+
+    private UnitType unitType;
+    private int amountInStock = 0;
+    private bool isBelowThreshold = false;
+}
---
+++ ShalabySweets.InventoryManagement/UnitType.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+public enum UnitType
+{
+    PerItem,
+    PerBox,
+    PerKg
+}
